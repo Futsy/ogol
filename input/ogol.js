1aa3b2329b8581cd08849a70526fa3a219fc554f
Futsy <florisvelleman@hotmail.com>
2015-08-20 16:05:34 +0200

    ogol: added exercise for functions

--- input/ogol.js
+++ input/ogol.js
@@ -2,9 +2,21 @@ function render() {
   var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
   ctx.beginPath();
+  ctx.moveTo(313, 313);
+  ctx.lineTo(313, 213);
+  ctx.moveTo(313, 213);
+  ctx.lineTo(213, 213);
+  ctx.moveTo(213, 213);
+  ctx.lineTo(213, 313);
+  ctx.moveTo(213, 313);
+  ctx.lineTo(313, 313);
   ctx.moveTo(250, 250);
-  ctx.lineTo(260, 260);
-  ctx.moveTo(280, 290);
-  ctx.lineTo(350, 360);
+  ctx.lineTo(350, 250);
+  ctx.moveTo(350, 250);
+  ctx.lineTo(350, 390);
+  ctx.moveTo(350, 390);
+  ctx.lineTo(250, 390);
+  ctx.moveTo(250, 390);
+  ctx.lineTo(250, 250);
   ctx.stroke();
 }
